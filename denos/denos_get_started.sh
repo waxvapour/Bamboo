0da864cc6c699733db0faa6e38e21c5d213d5cfa
waxvapour <waxvapour@gmaail.com>
2020-11-20 09:31:01 +0530

    denOS Respin project

---
+++ denos/denos_get_started.sh
@@ -0,0 +1,36 @@
+# DenOS - Debian Linux build from scratch with Debootstrap#
+# Bernardino Lopez [ bernardino.lopez@gmail.com ]
+# 11.27.18
+
+# denos_get_started.sh - Initial Pre-Req Download Script.
+# Execute the following command in a Terminal to get started the DenOS download scripts...
+# wget https://raw.githubusercontent.com/dinooz/denos/master/denos_get_started.sh
+# chmod 755 denos_get_started.sh
+# ./denos_get_started.sh
+
+wget https://github.com/dinooz/denos/raw/master/Denos_HowTo.txt
+wget https://github.com/dinooz/denos/raw/master/01_denos_pre_reqs.sh
+wget https://github.com/dinooz/denos/raw/master/02_denos_debootstrap.sh
+
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_lxde.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_xfce.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_kde.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_mate.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_plasma.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_e17.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_i3.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_openbox.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_awesome.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_lxqt.sh
+# Working on this scripts...
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_cinnamon.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_pantheon.sh
+wget https://github.com/dinooz/denos/raw/master/03_denos_chroot_gnome.sh
+
+wget https://github.com/dinooz/denos/raw/master/04_denos_MakeSquashfs.sh
+wget https://github.com/dinooz/denos/raw/master/05_denos_BiosUEFI_ISO.sh
+wget https://github.com/dinooz/denos/raw/master/denos_config.txt
+wget https://github.com/dinooz/denos/raw/master/denos_clean.sh
+
+chmod 755 *.sh
